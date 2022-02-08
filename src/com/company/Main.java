7ecd9234122c764cf1e24f9acfdd24a220c697dc
Olga Em <olga.em020@gmail.com>
2022-02-09 02:04:54 +0700

    Added all

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int balance = 900;
+        int refill = 1300;
+        int oneBonus = 100;
+
+        if (refill > 1000){
+            int bonus = refill/oneBonus;
+            int finalScore = balance + refill + bonus;
+            System.out.println("Итоговый счет = " + finalScore);
+            System.out.println("Бонусных рублей начисленно " + bonus);
+        }else {
+            int finalScore = balance + refill;
+            System.out.println("Итоговый счет = " + finalScore);
+            System.out.println("Бонусные рубли не начислены");
+        }
+    }
+}
