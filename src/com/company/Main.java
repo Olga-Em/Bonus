88e854c4799eabf2c7cd64e6a6d60b776c2e35fc
Olga Em <olga.em020@gmail.com>
2022-03-19 15:29:02 +0700

    reformated code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,12 +7,13 @@ public class Main {
         int refill = 1300;
         int oneBonus = 100;
 
-        if (refill > 1000){
-            int bonus = refill/oneBonus;
+        if (refill > 1000) {
+            int bonus = refill / oneBonus;
             int finalScore = balance + refill + bonus;
             System.out.println("Итоговый счет = " + finalScore);
             System.out.println("Бонусных рублей начисленно " + bonus);
-        }else {
+        }
+        else {
             int finalScore = balance + refill;
             System.out.println("Итоговый счет = " + finalScore);
             System.out.println("Бонусные рубли не начислены");
